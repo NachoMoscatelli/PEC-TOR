f1d3b85c28d2b5ec9e969a61a1ac2ed5cb75e654
NachoMoscatelli <inm300300@gmail.com>
2026-01-30 22:01:47 -0300

    Kickoff: Suerte Vaqueros!

--- word.gd
+++ word.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
-@onready var player = $Actores/Player
+@onready var player = $Actors/Hector
 @onready var camera = $Camera
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	camera.position = player.position
+	camera.position.x = player.position.x
+	camera.position.y = player.position.y
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	camera.position = player.position
+	camera.position.x = player.position.x
+	camera.position.y = player.position.y - 20
+	
+	
 	#if player.position.x > camera.position.x:
 		#camera.position.x = player.position.x
